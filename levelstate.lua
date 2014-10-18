9212c2033e971d2687b54313492cd0890fd15619
Alex Bechanko <chanko08@gmail.com>
2014-10-18 14:20:28 -0400

    added signals, made simple player with physics

--- levelstate.lua
+++ levelstate.lua
@@ -1,20 +1,41 @@
-local LevelState = {}
+local Class  = require('hump.class')
+local Signal = require('hump.signal')
+
+local Player = require('player')
+
+
+local LevelState = Class({})
+local registry = nil
+local physics_world = nil
 
 function LevelState:init()
+    self.registry:emit('state_init', key)
+    self.registry = Signal.new()
+
+    love.physics.setMeter(64)
+    self.physics_world = love.physics.newWorld(0, 0, true)
+
+    self.player = Player.new(self, 0 , 0, 64)
 end
 
 function LevelState:enter( prev, ... )
+    self.registry:emit('state_enter', key)
 end
 
 function LevelState:leave()
+    self.registry:emit('state_leave', key)
 end
 
 function LevelState:update( dt )
     -- body
+    self.physics_world:update(dt)
+    self.registry:emit('update', key)
 end
 
 function LevelState:draw()
     -- body
+    self.registry:emit('draw', key)
+
 end
 
 function LevelState:keypressed( key )
@@ -22,18 +43,23 @@ function LevelState:keypressed( key )
     if key == 'escape' then
         love.event.quit()
     end
+
+    self.registry:emit('keypressed', key)
 end
 
 function LevelState:keyreleased( key )
     -- body
+    self.registry:emit('keyreleased', key)
 end
 
 function LevelState:mousepressed( x, y, button )
     -- body
+    self.registry:emit('mousepressed', x, y, button)
 end
 
 function LevelState:mousereleased( x, y, button )
     -- body
+    self.registry:emit('mousereleased', x, y, button)
 end
 
 return LevelState
---
+++ player.lua
@@ -0,0 +1,16 @@
+local Class = require('hump.class')
+
+local Player = Class({})
+
+function Player:init(state, initial_x, initial_y, beginning_radius)
+
+    local body = love.physics.newBody(state.physics_world, initial_x, initial_y)
+    local shape = love.physics.newCircleShape(beginning_radius)
+
+    self.physics = love.physics.newFixture(body, shape, density)
+
+    
+end
+
+
+
